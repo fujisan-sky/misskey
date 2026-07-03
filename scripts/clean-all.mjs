eb23e4a8d49193172824e14a2a8f01ea422cb3cd
mikop <saba1983332293@misskey.day>
2026-07-03 14:08:07 +0900

    fujisan2026.6.0h

--- scripts/clean-all.mjs
+++ scripts/clean-all.mjs
@@ -3,15 +3,19 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-const { execSync } = require('child_process');
-const fs = require('fs');
+import { execSync } from 'node:child_process';
+import * as fs from 'node:fs';
+
+const __dirname = import.meta.dirname;
 
 (async () => {
 	fs.rmSync(__dirname + '/../packages/backend/built', { recursive: true, force: true });
+	fs.rmSync(__dirname + '/../packages/backend/src-js', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/backend/node_modules', { recursive: true, force: true });
 
-	fs.rmSync(__dirname + '/../packages/frontend-shared/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/frontend-shared/node_modules', { recursive: true, force: true });
+
+	fs.rmSync(__dirname + '/../packages/frontend-builder/node_modules', { recursive: true, force: true });
 
 	fs.rmSync(__dirname + '/../packages/frontend/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/frontend/node_modules', { recursive: true, force: true });
@@ -21,6 +25,9 @@ const fs = require('fs');
 
 	fs.rmSync(__dirname + '/../packages/sw/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/sw/node_modules', { recursive: true, force: true });
+
+	fs.rmSync(__dirname + '/../packages/i18n/built', { recursive: true, force: true });
+	fs.rmSync(__dirname + '/../packages/i18n/node_modules', { recursive: true, force: true });
 
 	fs.rmSync(__dirname + '/../packages/misskey-js/built', { recursive: true, force: true });
 	fs.rmSync(__dirname + '/../packages/misskey-js/node_modules', { recursive: true, force: true });
